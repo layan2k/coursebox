0dfcc154293f079bd92d436b1cbfd959b4619c0a
layan2k <lesliedeeshumba@gmail.com>
2023-10-14 22:48:17 +0200

    add testing-library

--- src/app/components/Button/Button.stories.tsx
+++ src/app/components/Button/Button.stories.tsx
@@ -5,15 +5,27 @@ import React from 'react';
 const meta = {
     title: "Controls/Button",
     component: Button,
+    args: {
+        children:'Click Me'
+    }
 } as Meta<typeof Button>
 
 export default meta
-const Template: StoryFn<typeof Button> = (args) => <Button {...args} />;
 
-export const BasicButton = Template.bind({});
-BasicButton.args = {
-    children: "Button"
+export const Primary = {
+    args : {
+        primary: true,
+    }
 }
+
+export const LargePrimary = {
+    args: {
+        ...Primary.args,
+        size: 'large',
+        children: "More To Click Me"
+    }
+}
+
 
 
 
